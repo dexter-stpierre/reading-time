0b6de8c8dbeef108fee549a8574bcd32447f6e53
Dexter St Pierre <dexter.stpierre.dev@gmail.com>
2023-09-01 21:40:34 -0500

    Update dependencies

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 import { Button } from './components/Button'
-import { Link, NativeRouter, Route, Routes } from "react-router-native";
-import { List } from './pages/List';
-import { Book } from './pages/Book';
+import { Link, NativeRouter, Route, Routes } from 'react-router-native'
+import { List } from './pages/List'
+import { Book } from './pages/Book'
+import React from 'react'
 
 export default function App() {
   return (
@@ -11,19 +12,19 @@ export default function App() {
       <View style={styles.container}>
         <Routes>
           <Route path='/' element={<Text>Hello World</Text>} />
-            <Route path="/list" element={<List />} />
-            <Route path="/book" element={<Book />} />
-          </Routes>
+          <Route path='/list' element={<List />} />
+          <Route path='/book' element={<Book />} />
+        </Routes>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>This is something I wrote</Text>
         <Button onPress={() => alert('button has been pressed')}>
           <Text>Add a new book</Text>
         </Button>
         <Navigation />
-        <StatusBar style="auto" />
+        <StatusBar style='auto' />
       </View>
     </NativeRouter>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -33,11 +34,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
 
 const Navigation = () => {
-  return <View>
-    <Link to='/list'><Text>Go to List</Text></Link>
-    <Link to='/book'><Text>Go to Book</Text></Link>
-  </View>
+  return (
+    <View>
+      <Link to='/list'>
+        <Text>Go to List</Text>
+      </Link>
+      <Link to='/book'>
+        <Text>Go to Book</Text>
+      </Link>
+    </View>
+  )
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,8 @@
-import { PropsWithChildren } from 'react';
-import { StyleSheet, Text, Pressable, View, PressableProps } from 'react-native';
+import React from 'react'
+import { PropsWithChildren } from 'react'
+import { StyleSheet, Text, Pressable, View, PressableProps } from 'react-native'
 
-type ButtonProps = Pick<PressableProps, 'onPress'> & PropsWithChildren<{ }>
+type ButtonProps = Pick<PressableProps, 'onPress'> & PropsWithChildren<{}>
 
 export function Button({ onPress, children }: ButtonProps) {
   return (
@@ -10,7 +11,7 @@ export function Button({ onPress, children }: ButtonProps) {
         <Text>{children}</Text>
       </Pressable>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,6 +19,6 @@ const styles = StyleSheet.create({
     padding: 8,
     borderWidth: 1,
     borderColor: 'black',
-    borderRadius: 10
+    borderRadius: 10,
   },
-});
+})
